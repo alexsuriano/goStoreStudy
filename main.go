dc2c0fbcfc94cfa05e8c117db0b7ab61ae3b5cdb
alexandre.suriano@gmail.com <d31m083649243502>
2021-06-25 20:42:53 -0300

    function to add products dynamically and improvements in index.html

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,12 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Produto novo", "Muito legal", 1.99, 1},
+	}
+
+	templates.ExecuteTemplate(w, "Index", produtos)
 }
